245c5fd488171b0673255eea62cc29fb84fb7f22
Kyle Whitaker <kylewhitaker51@gmail.com>
2019-06-12 12:15:12 -0400

    convert to SCSS to override bootstrap theme colors

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html'
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  themeColors = [
+    'primary',
+    // 'secondary',
+    // 'success',
+    // 'info',
+    // 'warning',
+    // 'danger',
+    // 'light',
+    // 'dark'
+  ];
+
+  isDark(color: string): boolean {
+    return color === 'dark' || color === 'danger';
+  }
+}
