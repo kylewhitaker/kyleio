e8d6d8ef1ac27661a69f21976d3230c1afbdbf01
Kyle Whitaker <kylewhitaker51@gmail.com>
2019-06-12 15:00:29 -0400

    started adding home welcome section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     NavbarComponent
   ],
   imports: [
