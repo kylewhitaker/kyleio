a78191a5eac117ef5e9c0bd51b036f69c5a82a87
Kyle Whitaker <kylewhitaker51@gmail.com>
2019-06-16 22:37:51 -0400

    data-bind contact form to api call

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 import { environment } from '../../../environments/environment';
+import { Contact } from '../../models/contact.model';
 
 @Component({
   selector: 'app-contact',
@@ -8,21 +10,16 @@ import { environment } from '../../../environments/environment';
 })
 export class ContactComponent {
 
+  contact = new Contact('', '', '', '');
+
   constructor(private http: HttpClient) { }
 
   sendEmail(): void {
-    const body = {
-      name: 'jabroni smith',
-      email: 'jabroni.smith@gmail.com',
-      subject: 'Hire you!',
-      message: 'When can you start?'
-    };
-
     const baseUrl = environment.production
       ? 'https://enigmatic-lowlands-97259.herokuapp.com'
       : 'http://localhost:8080';
 
-    this.http.post(`${baseUrl}/email`, body).subscribe({
+    this.http.post(`${baseUrl}/email`, this.contact).subscribe({
       next: (response) => console.log('success:', response),
       error: (err) => console.log('error:', err)
     });
