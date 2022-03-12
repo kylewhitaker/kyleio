053d6b5ffc26d38c44a5d819b06bb23824f708e1
Kyle Whitaker <kwhitaker@level20.com>
2022-03-11 22:32:37 -0500

    prolife2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,42 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import {
+  Routes,
+  RouterModule,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from "@angular/router";
 
-import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { JumbotronComponent } from "./components/jumbotron/jumbotron.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { PortfolioComponent } from "./components/portfolio/portfolio.component";
 
 const routes: Routes = [
-  { path: '', component: JumbotronComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: 'portfolio', component: PortfolioComponent },
-  { path: '**', redirectTo: '' }
+  { path: "", component: JumbotronComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "portfolio", component: PortfolioComponent },
+  {
+    path: "prolife2022",
+    component: JumbotronComponent,
+    resolve: {
+      url: "externalUrlRedirectResolver",
+    },
+    data: {
+      externalUrl: "https://wp.kylewhitaker.io/",
+    },
+  },
+  { path: "**", redirectTo: "" },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    {
+      provide: "externalUrlRedirectResolver",
+      useValue: (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+        window.location.href = (route.data as any).externalUrl;
+      },
+    },
+  ],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
